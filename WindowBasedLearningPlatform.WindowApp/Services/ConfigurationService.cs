e65f3d7c8642845e50e3a37300a9a5a52dca6078
Nay Khant Kyaw <naykhantkyaw.coding@gmail.com>
2025-12-28 22:32:33 +0630

    feat: adding Configuration service

--- WindowBasedLearningPlatform.WindowApp/Services/ConfigurationService.cs
+++ WindowBasedLearningPlatform.WindowApp/Services/ConfigurationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,26 @@ namespace WindowBasedLearningPlatform.WindowApp.Services
 {
     public class ConfigurationService
     {
-        
+        public static string GetDbConnection()
+        {
+            string connectionString = string.Empty;
+            JObject obj = new JObject();
+            try
+            {
+                string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config", "customsetting.json");
+                if (File.Exists(jsonPath))
+                {
+                    string json = File.ReadAllText(jsonPath);
+                    obj = JObject.Parse(json);
+
+                    connectionString = obj["ConnectionStrings"]["DefaultConnection"].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return connectionString;
+        }
     }
 }
---
+++ WindowBasedLearningPlatform.WindowApp/Services/DatabaseService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowBasedLearningPlatform.WindowApp.Services
+{
+    public class DatabaseService
+    {
+        // for select data
+        public List<T> Query<T>(string query,params SqlParameter[] parameters)
+        {
+            string connectionString = ConfigurationService.GetDbConnection();
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(query,connection);
+
+            if(parameters is not null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            connection.Close();
+            string json = JsonConvert.SerializeObject(dt);
+            List<T>? data = JsonConvert.DeserializeObject<List<T>>(json);
+            return data;
+        }
+    }
+}
