af847a353f8b5ab55a5886f8c7d09a8e12619b09
Nay Khant Kyaw <naykhantkyaw.coding@gmail.com>
2025-12-28 22:41:27 +0630

    feat: adding UserModel

---
+++ WindowBasedLearningPlatform.WindowApp/Models/UserModel/UserRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowBasedLearningPlatform.WindowApp.Models.UserModel
+{
+    public class UserRequestModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ WindowBasedLearningPlatform.WindowApp/Models/UserModel/UserResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowBasedLearningPlatform.WindowApp.Models.UserModel
+{
+    public class UserResponseModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
