05b5aef776bd4dcb725e3785dae6d82abd2b5396
Nay Khant Kyaw <naykhantkyaw.coding@gmail.com>
2025-12-28 22:38:42 +0630

    feat: adding db service

--- WindowBasedLearningPlatform.WindowApp/Services/DatabaseService.cs
+++ WindowBasedLearningPlatform.WindowApp/Services/DatabaseService.cs
@@ -12,14 +12,14 @@ namespace WindowBasedLearningPlatform.WindowApp.Services
     public class DatabaseService
     {
         // for select data
-        public List<T> Query<T>(string query,params SqlParameter[] parameters)
+        public List<T> Query<T>(string query, params SqlParameter[] parameters)
         {
             string connectionString = ConfigurationService.GetDbConnection();
             SqlConnection connection = new SqlConnection(connectionString);
             connection.Open();
-            SqlCommand command = new SqlCommand(query,connection);
+            SqlCommand command = new SqlCommand(query, connection);
 
-            if(parameters is not null)
+            if (parameters is not null)
             {
                 command.Parameters.AddRange(parameters);
             }
@@ -31,6 +31,46 @@ namespace WindowBasedLearningPlatform.WindowApp.Services
             string json = JsonConvert.SerializeObject(dt);
             List<T>? data = JsonConvert.DeserializeObject<List<T>>(json);
             return data;
+        }
+
+        // for select data with specific range
+        public T? QueryFirstOrDefault<T>(string query, params SqlParameter[] parameters)
+        {
+            string connectionString = ConfigurationService.GetDbConnection();
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+
+            if (parameters is not null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            connection.Close();
+            string json = JsonConvert.SerializeObject(dt);
+            List<T>? data = JsonConvert.DeserializeObject<List<T>>(json);
+            return data[0];
+        }
+
+        // for insert,update,delete
+        public int Execute(string query, params SqlParameter[] parameters)
+        {
+            string connectionString = ConfigurationService.GetDbConnection();
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+
+            if (parameters is not null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+
+            var result = command.ExecuteNonQuery();
+            connection.Close();
+            return result;
         }
     }
 }
