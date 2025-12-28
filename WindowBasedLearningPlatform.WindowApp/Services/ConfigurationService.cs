1291904e007f3728cf4258abeac616d0f278d7af
Nay Khant Kyaw <naykhantkyaw.coding@gmail.com>
2025-12-28 21:46:38 +0630

    feat: adding json package and sql client package

--- WindowBasedLearningPlatform.WindowApp/Services/ConfigurationService.cs
+++ WindowBasedLearningPlatform.WindowApp/Services/ConfigurationService.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace WindowBasedLearningPlatform.WindowApp.Services
 {
-    public class Class1
+    public class ConfigurationService
     {
+        
     }
 }
